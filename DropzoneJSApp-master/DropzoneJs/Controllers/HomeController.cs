c875392b0a733aca008404ed9c39b939dfdb8573
Cathorlin <42290584+Cathorlin@users.noreply.github.com>
2018-11-29 10:40:59 +0800

    拖拽上传文件

---
+++ DropzoneJSApp-master/DropzoneJs/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DropzoneJs.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        /// <summary>
+        /// to Save DropzoneJs Uploaded Files
+        /// </summary>
+        public ActionResult SaveDropzoneJsUploadedFiles()
+        {
+            foreach (string fileName in Request.Files)
+            {
+                HttpPostedFileBase file = Request.Files[fileName];
+                //You can Save the file content here
+                var uploadFileName = file.FileName;
+
+                string filePath = "/Files/" + uploadFileName;
+                string AbsolutePath = Server.MapPath(filePath);
+                file.SaveAs(AbsolutePath);              //将上传的东西保存      
+            }
+
+            return Json(new { Message = string.Empty });
+        }
+    }
+}
